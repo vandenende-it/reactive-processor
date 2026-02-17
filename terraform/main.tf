8553add5b0c0d9d2fc0d86ff60a99319bf5ef87d
Maarten van den Ende <maarten@vandenende.it>
2026-02-17 19:09:28 +0100

    gen app

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,6 @@ provider "helm" {
   }
 }
 
-# Maak een namespace voor ArgoCD
 resource "kubernetes_namespace" "argocd" {
   metadata { name = "argocd" }
 }
@@ -38,4 +37,78 @@ resource "kubernetes_namespace" "app_space" {
   metadata {
     name = "vandenende-reactive"
   }
+}
+
+resource "helm_release" "redpanda" {
+  name       = "redpanda"
+  repository = "https://charts.redpanda.com"
+  chart      = "redpanda"
+  namespace  = kubernetes_namespace.app_space.metadata[0].name
+
+  set = [
+    {
+      name  = "statefulset.replicas"
+      value = "1"
+    },
+    {
+      name  = "storage.persistentVolume.enabled"
+      value = "false"
+    },
+    {
+      name  = "console.enabled"
+      value = "true"
+    },
+    {
+      name  = "tuning.tune_aio_events"
+      value = "false"
+    },
+    # --- DEZE ZIJN NU CRUCIAAL ---
+    {
+      name  = "tls.enabled"
+      value = "false"
+    },
+    {
+      name  = "external.enabled"
+      value = "false"
+    },
+    {
+      name  = "config.cluster.auto_create_topics_enabled"
+      value = "true"
+    }
+  ]
+}
+
+resource "kubernetes_manifest" "argocd_app" {
+  # We voegen deze 'field_manager' toe om conflicten met handmatige acties in de UI op te lossen
+  field_manager {
+    force_conflicts = true
+  }
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "reactive-processor"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/vandenende-it/reactive-processor.git"
+        targetRevision = "HEAD"
+        path           = "charts/reactive-processor"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "vandenende-reactive"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+  depends_on = [helm_release.argocd]
 }
